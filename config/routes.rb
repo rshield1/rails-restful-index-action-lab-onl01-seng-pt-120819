ef61b23e2a719313eaa0224283dc292176c062ed
Robert Shields <robert.shieldsiv@gmail.com>
2020-04-20 17:47:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   
-  get 'index', to: 'static#index'
+  resources :students, only: :index
 end
